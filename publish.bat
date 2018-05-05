42daa77943de8ff5e4e26bebd97c82585eaf1c58
zushouqq <godbog@163.com>
2018-05-05 12:13:13 +0800

    [ci skip] build new index
    Signed-off-by: zushouqq <godbog@163.com>

--- publish.bat
+++ publish.bat
@@ -1,5 +1,2 @@
 cd C:\Documents and Settings\Administrator\My Documents\Downloads\ggqqd_com
-git add .
-git commit -s -m \"[ci skip] build new index\"
-git push -u origin master
-pause
+python autoindex.py PUSH
